722b875c2b39ffd62e3d5b4cd023d9d91532beb0
Tony Linz <tonyllinz@gmail.com>
2021-12-23 00:49:21 -0600

    transition to high scores works

--- assets/script.js
+++ assets/script.js
@@ -46,7 +46,7 @@ var questionFive = {
 };
 
 var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
-
+var scoreList = [];
 var myTimer = document.querySelector("#timer");
 var timer = 60;
 var questionNumber = 0;
@@ -124,25 +124,38 @@ var createCorrectnessDiv = function() {
 }
 
 var correctCheck = function(event) {
+
+    //after work, pick up here, we're going to do an if check to see if the event.target
+    //is an answer class. this should allow the submit button to only activate
+    //high scores event. this solves our bubbling issue and the console log
+    //error from it trying to do a correct check and the questionObj of an array
+    //that doesn't have anything in the 6 index.
     //when one of the .answer class is clicked, display wrong or incorrect in .correctness div
+
     var targetEl = event.target;
-    var questionObj = questions[questionNumber];
-    var correctAnswerId= questionObj.correct;
-    var answerID = targetEl.id;
 
-    if (targetEl.matches(".answer") && answerID==correctAnswerId) {
-        correctTransitionHelper();
-        var correctnessDiv = document.querySelector(".correctness");
-        correctnessDiv.innerHTML = "Correct!";    
+    if (targetEl.matches(".submit-initials") || targetEl.matches(".initials-input")) {
+        return;
     }
+    else {
+        var answerID = targetEl.id;
+        var questionObj = questions[questionNumber];
+        var correctAnswerId= questionObj.correct;
 
-    else if (targetEl.matches(".answer") && answerID!=correctAnswerId) {
-        correctTransitionHelper();
-        var correctnessDiv = document.querySelector(".correctness");
-        correctnessDiv.innerHTML = "Wrong!";
-    }
+        if (targetEl.matches(".answer") && answerID==correctAnswerId) {
+            correctTransitionHelper();
+            var correctnessDiv = document.querySelector(".correctness");
+            correctnessDiv.innerHTML = "Correct!";    
+        }
 
-    endGameCheck();
+        else if (targetEl.matches(".answer") && answerID!=correctAnswerId) {
+            correctTransitionHelper();
+            var correctnessDiv = document.querySelector(".correctness");
+            correctnessDiv.innerHTML = "Wrong!";
+        }
+
+        endGameCheck();
+    } 
 }
 
 var correctTransitionHelper = function() {
@@ -166,6 +179,14 @@ var clearMain = function() {
     mainSection.innerHTML="";
 }
 
+var clearHeader = function() {
+    //finish this
+}
+
+var saveSCores = function() {
+    //finish this
+}
+
 var endGameCheck = function() {
     if (questionNumber==5) {
         questionNumber++;
@@ -174,6 +195,7 @@ var endGameCheck = function() {
     }
 }
 
+//dynamically alter html for transition to all done page
 var allDone = function() {
     clearMain();
     var allDoneMain = document.querySelector("#main-section");
@@ -194,6 +216,7 @@ var allDone = function() {
 
     //create a form element for form css and methods
     formEl = document.createElement("form");
+    formEl.className = "submit-initials";
     formElDiv.appendChild(formEl);
 
     //create h2 prompt to enter initials
@@ -203,8 +226,9 @@ var allDone = function() {
 
     //create a div to put the input in because I don't
     formElInput = document.createElement("input");
-    formElInput.setAttribute('type', 'text')
-    formElInput.setAttribute('name', 'player-initials')
+    formElInput.className = "initials-input";
+    formElInput.setAttribute('type', 'text');
+    formElInput.setAttribute('name', 'player-initials');
     formElInput.setAttribute('placeholder', 'Enter initials');
     formEl.appendChild(formElInput);
     
@@ -212,13 +236,60 @@ var allDone = function() {
     formElButton.textContent = "Submit";
     formElButton.className = "submit-initials selectable";
     formEl.appendChild(formElButton);
+}
+
+//dynamically alter html for transition to highscores page
+var highScores = function(event) {
+
+    event.preventDefault();
+
+    saveScores();
+
+    var submitEl = event.target;
+    var initialsInput = document.querySelector("input[name='player-initials']").value;
+    var currentScore = document.querySelector("input[")
+
+    //if you click on the submit button on the all done page, transition to high scores page
+    if (submitEl.matches('.submit-initials')) {
+        clearMain();
+        clearHeader();
+        var allDoneMain = document.querySelector("#main-section");
+
+        //create h1 for highscores
+        var h1El = document.createElement("h1");
+        h1El.innerText = "High Scores!";
+        allDoneMain.appendChild(h1El);
+
+        //create div to display high score and initials
+        var highScoreSection = document.createElement("section");
+
+        // if timer is less than all the other values in scoreList
+
+
+        for (var i=0; i<scoreList.length; i++) {
+            var scoreDiv = document.createElement("div");
+            //find a way to add rank. initials - score
+            scoreDiv.innerHTML = ""
+            highScoreSection.append(scoreDiv);
+        }
+
+        //create go back button an link it to the entering your initials page
+
+
+        //create clear high scores button
+    }  
 
 }
 
-//when I click a button to move on from the question slides
 
+//when I click a button to move on from the question slides
 mainSection.addEventListener("click", startToQuestions);
 
 //when I select an answer to a question
-
 mainSection.addEventListener("click", correctCheck);
+
+//when I click on the button with the class 'submit-initials, transtion to high scores page
+mainSection.addEventListener("submit", highScores);
+
+//don't forget to add event.preventdefault when you submit the form
+
